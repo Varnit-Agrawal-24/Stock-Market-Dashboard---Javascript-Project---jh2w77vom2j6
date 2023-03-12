67415b6162ae654b32843a7e81dd3a102bb79cae
Varnit Agrawal <varnit.agrawal24@gmail.com>
2023-03-12 14:06:44 +0530

    api working

--- App.js
+++ App.js
@@ -1,1 +1,23 @@
+const api_key='DA452BSDFSBL814Z';
+let submit=document.querySelector("#submit");
+
+submit.addEventListener("click",()=>{
+    let time=document.querySelector("#time-series").value;
+    let symbol=document.querySelector("#symbol").value.toUpperCase();
+    let interval="";
+    if(time=="TIME_SERIES_INTRADAY"){
+        interval="interval=15min&";
+    } 
+    const api=`https://www.alphavantage.co/query?function=${time}&symbol=${symbol}&${interval}apikey=${api_key}`;
+
+    fetch(api)
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+       console.log(data);
+    })
+    document.querySelector("#time-series").value="none";
+    document.querySelector("#symbol").value="";
+});
 
